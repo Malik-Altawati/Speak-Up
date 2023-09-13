8f77bed47e0694a9b1b34e1af943e7bb98be8b54
Malik Altawati <MalikAltawati@gmail.com>
2023-09-13 05:34:10 -0400

    update

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SubmitFormComponent } from './submit-form/submit-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:SubmitFormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SubmitFormComponent } from './submit-form/submit-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SubmitFormComponent
   ],
   imports: [
     BrowserModule,
